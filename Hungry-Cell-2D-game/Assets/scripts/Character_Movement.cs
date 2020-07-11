a3cdabfce50dd5ff47164ca6c5262ace4ef9770f
OverLordN7 <arifdjanovsarvar@gmail.com>
2020-07-11 13:39:40 +0500

    Hungry-Cell-2D v 0.01
    made simple 2D movement for player

--- Hungry-Cell-2D-game/Assets/scripts/Character_Movement.cs
+++ Hungry-Cell-2D-game/Assets/scripts/Character_Movement.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Character_Movement : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+    public float speed = 10f;
+    public float xmove, ymove = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        xmove = Input.GetAxisRaw("Horizontal")* speed;
+        ymove = Input.GetAxisRaw("Vertical") * speed;
+        Vector2 movement = new Vector2(xmove,ymove);
+        rb2d.AddForce(movement);
     }
 }
