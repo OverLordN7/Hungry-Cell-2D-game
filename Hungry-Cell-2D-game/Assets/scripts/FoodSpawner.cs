6e326075a3fbd3dad534a7fc323138d1b4aeed09
OverLordN7 <arifdjanovsarvar@gmail.com>
2020-07-13 11:39:19 +0500

    Hungry-Cell-2D v 0.04.1
    added primitive spawn system

---
+++ Hungry-Cell-2D-game/Assets/scripts/FoodSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FoodSpawner : MonoBehaviour
+{
+    public GameObject foodPrefab;
+
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+    private bool gameStarted = true;
+
+    private void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,
+            Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(FoodWave());
+    }
+
+    private void spawnFood()
+    {
+        GameObject a = Instantiate(foodPrefab) as GameObject;
+        a.transform.position = new Vector2(Random.Range(-screenBounds.x+5, screenBounds.x-5), 
+            Random.Range(-screenBounds.y + 2, screenBounds.y - 2));
+        Destroy(a,2.5f);
+    }
+
+    IEnumerator FoodWave()
+    {
+        while (gameStarted)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnFood();
+        }
+    }
+}
