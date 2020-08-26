388dbfa7a5147d4ced5c7fba3b603cab0fee343d
OverLordN7 <arifdjanovsarvar@gmail.com>
2020-08-26 20:50:29 +0500

    Hungry-Cell-2D v 0.21.2
    Experiments with joystick and alternative touch movement

---
+++ Hungry-Cell-2D-game/Assets/scripts/CharacterActions/TouchControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class TouchControl : MonoBehaviour
+{
+   private float speed =5;
+   private Vector3 targetPosition;
+   private bool isMoving = false;
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SetTargetPosition();
+        }
+
+        if (isMoving)
+        {
+            Move();
+        }
+        
+    }
+
+    void SetTargetPosition()
+    {
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition.z = transform.position.z;
+        isMoving = true;
+    }
+
+    void Move()
+    {
+        transform.rotation= Quaternion.LookRotation(Vector3.forward,targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if (transform.position == targetPosition)
+        {
+            isMoving=false;
+        }
+    }
+}
